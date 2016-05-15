0bcc60dc96da3532e31b0d2a29f7051e844724fc
Drew Harding <andrew.harding@btfinancialgroup.com>
2016-05-15 12:49:05 +1000

    clean up crappy formatting of go code from vehicle demo

--- micromarket.go
+++ micromarket.go
@@ -145,70 +145,85 @@ func (t *Chaincode) get_user_data(stub *shim.ChaincodeStub, name string) ([]byte
 // 				  certificates extensions containing the role before returning the role interger. Returns -1 if it errors
 //==============================================================================================================================
 func (t *Chaincode) check_role(stub *shim.ChaincodeStub, args []string) (int64, error) {																							
-	ECertSubjectRole := asn1.ObjectIdentifier{2, 1, 3, 4, 5, 6, 7}																														
-	
-	decodedCert, err := url.QueryUnescape(args[0]);    		// make % etc normal //
-	
-															if err != nil { return -1, errors.New("Could not decode certificate") }
-	
-	pem, _ := pem.Decode([]byte(decodedCert))           	// Make Plain text   //
+    ECertSubjectRole := asn1.ObjectIdentifier{2, 1, 3, 4, 5, 6, 7}																														
 
-	x509Cert, err := x509.ParseCertificate(pem.Bytes);		// Extract Certificate from argument //
-														
-															if err != nil { return -1, errors.New("Couldn't parse certificate")	}
+    // make % etc normal
+    decodedCert, err := url.QueryUnescape(args[0]);
+    if err != nil { return -1, errors.New("Could not decode certificate") }
 
-	var role int64
-	for _, ext := range x509Cert.Extensions {				// Get Role out of Certificate and return it //
-		if reflect.DeepEqual(ext.Id, ECertSubjectRole) {
-			role, err = strconv.ParseInt(string(ext.Value), 10, len(ext.Value)*8)   
-                                            			
-															if err != nil { return -1, errors.New("Failed parsing role: " + err.Error())	}
-			break
-		}
-	}
-	
-	return role, nil
+    // Make Plain text
+    pem, _ := pem.Decode([]byte(decodedCert))
+
+    // Extract Certificate from argument
+    x509Cert, err := x509.ParseCertificate(pem.Bytes);
+    if err != nil {
+        return -1, errors.New("Couldn't parse certificate")
+    }
+
+    // Get Role out of Certificate and return it
+    var role int64
+    for _, ext := range x509Cert.Extensions {
+        if reflect.DeepEqual(ext.Id, ECertSubjectRole) {
+            role, err = strconv.ParseInt(string(ext.Value), 10, len(ext.Value)*8)   
+            if err != nil {
+                return -1, errors.New("Failed parsing role: " + err.Error())
+            }
+            break
+        }
+    }
+
+    return role, nil
 }
+
 //==============================================================================================================================
 //	 get_user - Takes an ecert, decodes it to remove html encoding then parses it and gets the
 // 				common name and returns it
 //==============================================================================================================================
 func (t *Chaincode) get_user(stub *shim.ChaincodeStub, encodedCert string) (string, error) {
-	
-	decodedCert, err := url.QueryUnescape(encodedCert);    		// make % etc normal //
-	
-															if err != nil { return "", errors.New("Could not decode certificate") }
-	
-	pem, _ := pem.Decode([]byte(decodedCert))           	// Make Plain text   //
+    // make % etc normal 
+    decodedCert, err := url.QueryUnescape(encodedCert);
+    if err != nil {
+        return "", errors.New("Could not decode certificate")
+    }
 
-	x509Cert, err := x509.ParseCertificate(pem.Bytes);
-	
-															if err != nil { return "", errors.New("Couldn't parse certificate")	}
+    // Make Plain text
+    pem, _ := pem.Decode([]byte(decodedCert))
+    x509Cert, err := x509.ParseCertificate(pem.Bytes);
+    if err != nil {
+        return "", errors.New("Couldn't parse certificate")
+    }
 
-	return x509Cert.Subject.CommonName, nil
-
+    // return the user from the certificate
+    return x509Cert.Subject.CommonName, nil
 }
 
 //==============================================================================================================================
 //	 get_ecert - Takes the name passed and calls out to the REST API for HyperLedger to retrieve the ecert
 //				 for that user. Returns the ecert as retrived including html encoding.
 //==============================================================================================================================
-func (t *Chaincode) get_ecert(stub *shim.ChaincodeStub, name string) ([]byte, error) {
-	
-	var cert ECertResponse
-	
-	response, err := http.Get("BLC_API_URL/registrar/"+name+"/ecert") // Calls out to the HyperLedger REST API to get the ecert of the user with that name
-    
-															if err != nil { return nil, errors.New("Could not get ecert") }
-	
-	defer response.Body.Close()
-	contents, err := ioutil.ReadAll(response.Body)			// Read the response from the http callout into the variable contents
-	
-															if err != nil { return nil, errors.New("Could not read body") }
-	
-	err = json.Unmarshal(contents, &cert)
-	
-															if err != nil { return nil, errors.New("ECert not found for user: "+name) }
-															
-	return []byte(string(cert.OK)), nil
+func (t *Chaincode) get_ecert(stub *shim.ChaincodeStub, name string) ([]byte, error) {	
+    var cert ECertResponse
+
+    // Calls out to the HyperLedger REST API to get the ecert of the user with that name
+    response, err := http.Get("BLC_API_URL/registrar/"+name+"/ecert")
+    if err != nil {
+        return nil, errors.New("Could not get ecert")
+    }
+
+    // use the defer construct to close the stream after the method completes
+    defer response.Body.Close()
+
+    // Read the response from the http callout into the variable contents
+    contents, err := ioutil.ReadAll(response.Body)
+    if err != nil {
+        return nil, errors.New("Could not read body")
+    }
+
+    // unmarshall the contents of the certificate
+    err = json.Unmarshal(contents, &cert)
+    if err != nil {
+        return nil, errors.New("ECert not found for user: "+name)
+    }
+
+    return []byte(string(cert.OK)), nil
 }
